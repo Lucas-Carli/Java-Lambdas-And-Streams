cd4e5bbc9981e390b1543734f6c1a1e983e9c2a6
lucas carli <lucas.carli@tecso.coop>
2025-03-11 16:56:54 -0300

    finish section 9 - Parallel Streams

---
+++ src/main/java/com/lambdas_and_streams/parallel_streams/ParallelStreamTimed.java
@@ -0,0 +1,16 @@
+package com.lambdas_and_streams.parallel_streams;
+
+import java.util.stream.IntStream;
+
+public class ParallelStreamTimed {
+
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+        IntStream.rangeClosed(1,2000000000).parallel().reduce(0, Integer::sum);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total time: " + (endTime - startTime));
+
+    }
+
+}
